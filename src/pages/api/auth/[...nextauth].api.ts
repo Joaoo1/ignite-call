8fed814d4318f6ee7ffb37f1d81dce724a3609b0
João V <silva.joao1@outlook.com.br>
2023-03-31 01:28:53 -0300

    feat: sign in on google

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -6,7 +6,23 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ account }) {
+      const hasPermissions = account?.scope?.includes(
+        'https://www.googleapis.com/auth/calendar',
+      )
+
+      return hasPermissions || '/register/connect-calendar?error=permissions'
+    },
+  },
 }
+
 export default NextAuth(authOptions)
