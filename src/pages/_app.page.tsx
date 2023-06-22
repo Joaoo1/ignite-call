59ce3758661633ffa7b2dc17ef350d938fcf8d6b
João V <silva.joao1@outlook.com.br>
2023-06-21 22:08:36 -0300

    feat: calendar availability

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,6 @@
 import '../lib/dayjs'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/react-query'
 
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
@@ -11,8 +13,10 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
